79431a1c5f7915b0934f49b9005e1621b065244a
Charles <charles@germanstarmotors.ca>
2019-04-08 17:02:33 -0400

    added fields

--- api/migrations/0014_auto_20190214_1711.py
+++ api/migrations/0014_auto_20190214_1711.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0020_auto_20190408_1546'),
+        ('api', '0013_auto_20190207_1642'),
     ]
 
     operations = [
