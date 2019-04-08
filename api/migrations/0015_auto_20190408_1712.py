501fb2d1180968fcd4fb751257a309d200245784
Charles <charles@germanstarmotors.ca>
2019-04-08 17:45:43 -0400

    added files

--- api/migrations/0015_auto_20190408_1712.py
+++ api/migrations/0015_auto_20190408_1712.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0014_auto_20190214_1711'),
+        ('api', '0001_initial'),
     ]
 
     operations = [
