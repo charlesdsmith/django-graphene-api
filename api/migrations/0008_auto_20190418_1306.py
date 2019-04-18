2e5a2131f6ca7f9ebf5bb25ab36cee8cf12f517c
Charles <charles@germanstarmotors.ca>
2019-04-18 17:41:45 -0400

    fixed migrations

--- api/migrations/0008_auto_20190418_1306.py
+++ api/migrations/0008_auto_20190418_1306.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0007_auto_20190417_1817'),
+        ('api', '0004_auto_20190409_1802'),
     ]
 
     operations = [
